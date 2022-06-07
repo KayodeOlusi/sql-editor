24bf57ae6aa04383c95c7723d7d43e73959b59b0
KayodeOusi <kayodeolusi@gmail.com>
2022-06-07 11:40:04 +0100

    cleaned up folders

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import { lazy, Suspense } from "react";
-import Spinner from "react-spinkit";
 import "./App.css";
-import SideMenu from "./components/Editor/SideMenu/SideMenu";
-
-const Container = lazy(() => import("./containers/Container"));
-const Header = lazy(() => import("./components/Header/Header"));
 
 function App() {
-  return (
-    <div className="App">
-      <Suspense
-        fallback={
-          <Spinner name="ball-clip-rotate-multiple" color="steelblue" />
-        }
-      >
-        <Container>
-          <Header />
-          <div>
-            <SideMenu />
-          </div>
-        </Container>
-      </Suspense>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
